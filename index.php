8a30dacef30f5b3953cc78364f4d0420145fba4e
EmmaSaill <32335799+EmmaSaill@users.noreply.github.com>
2017-11-10 10:33:25 +0800

    tex

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-$myfile = fopen("temp.txt", "r") or die("Unable to open file!");
-// 输出单字符直到 end-of-file
-while(!feof($myfile)) {
-  echo fgetc($myfile);
-}
-fclose($myfile);
-$a=file_get_contents('temp.txt');
-　　$a=preg_replace('/\n|\r\n/','bj',$a);
+
+$a=file_get_contents("temp.txt");
+　　$a=preg_replace("/\n|\r\n/","bj",$a);
 　　echo $a;
 ?>
